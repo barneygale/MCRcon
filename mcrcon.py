98a6e8dadd9f2d60a9b5499e5d43b8df57324700
Barney Gale <barney.gale@gmail.com>
2011-10-14 15:45:10 +0100

    No need to worry about incrementing request_id

--- mcrcon.py
+++ mcrcon.py
@@ -4,8 +4,6 @@ import struct
 import re
 
 class MCRcon:
-    token = 0
-    
     def __init__(self, host, port, password):
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((host, port))
@@ -17,16 +15,12 @@ class MCRcon:
     def send(self, command):
         return self.send_real(2, command)
     
-    def send_real(self, out_id, out_data):
-        #Get a new token
-        self.token += 1
-        out_token = self.token
-        
+    def send_real(self, out_type, out_data):
         #Send the data
         buff = struct.pack('iii', 
             10+len(out_data),
-            out_token,
-            out_id) + out_data + "\x00\x00"
+            0,
+            out_type) + out_data + "\x00\x00"
         self.s.send(buff)
         
         #Receive a response
@@ -34,18 +28,20 @@ class MCRcon:
         ready = True
         while ready:
             #Receive an item
-            tmp_len, tmp_token, tmp_id = struct.unpack('iii', self.s.recv(12))
+            tmp_len, tmp_req_id, tmp_type = struct.unpack('iii', self.s.recv(12))
             tmp_data = self.s.recv(tmp_len-8) #-8 because we've already read the 2nd and 3rd integer fields
 
-            #Strip pad bytes
-            assert tmp_data[-2:] == '\x00\x00'
+            #Error checking
+            if tmp_data[-2:] != '\x00\x00':
+                raise Exception('protocol failure', 'non-null pad bytes')
             tmp_data = tmp_data[:-2]
             
-            #Error-check
-            if tmp_token == -1:
+            #if tmp_type != out_type:
+            #    raise Exception('protocol failure', 'type mis-match', tmp_type, out_type)
+           
+            if tmp_req_id == -1:
                 raise Exception('auth failure')
-            elif tmp_token != out_token:
-                raise Exception('protocol error')
+           
             m = re.match('^Error executing: %s \((.*)\)$' % out_data, tmp_data)
             if m:
                 raise Exception('command failure', m.group(1))
