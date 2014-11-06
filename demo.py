111410e7e7b3f5636b2df9ca840f1f4020f7176c
Finnian Anderson <finnian@fxapi.co.uk>
2014-11-06 17:16:51 +0000

    Fix port

--- demo.py
+++ demo.py
@@ -3,12 +3,14 @@ import mcrcon
 
 print 'Ctrl-C to exit'
 host = raw_input('Host: ')
-if not host: host = "pimine.local"
+if not host:
+	host = "localhost"
 port = raw_input('Port (25575): ')
-if not port: port = 25566
-else: port = int(port)
+if not port:
+	port = 25575
+else:
+	port = int(port)
 pwd  = getpass.getpass('Password: ')
-if not pwd: pwd = "mc2014"
 
 print "Connecting..."
 server = mcrcon.MCRcon(host, port, pwd)
